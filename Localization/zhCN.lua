b6d074487e6c72e2b35b7e625af3675c7e7370ec
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-01-27 07:51:27 +0800

    Update zhCN.lua

--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -111,7 +111,7 @@ L.Settings.IndicateInterruptsLabel = "标记可打断法术"
 L.Settings.IndicateInterruptsTooltip =
 	"使图标去色，在图标上显示标记，并延迟1秒隐藏图标。对引导类法术无效。"
 
-L.Settings.ShowSwipeLabel = "Show Swipe"
+L.Settings.ShowSwipeLabel = "显示滑动"
 L.Settings.ShowSwipeTooltip = nil
 
 L.Settings.ShowBorderLabel = "显示边框"
