655144c690940990afbb835fce3e7c06071b22e4
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-01-27 20:30:15 +0800

    Update zhCN.lua

--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -96,8 +96,8 @@ L.Settings.GlowTypeTooltip = nil
 L.Settings.GlowTypeLabels = {
 	[Private.Enum.GlowType.PixelGlow] = "像素发光",
 	[Private.Enum.GlowType.AutoCastGlow] = "自动施法发光",
-	[Private.Enum.GlowType.ButtonGlow] = "按钮发光",
-	[Private.Enum.GlowType.ProcGlow] = "触发发光",
+	[Private.Enum.GlowType.ButtonGlow] = "快捷键发光",
+	[Private.Enum.GlowType.ProcGlow] = "脉冲发光",
 	[Private.Enum.GlowType.Star4] = "四星发光",
 }
 
